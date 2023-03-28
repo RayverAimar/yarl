94286bd54ab1ab4505a123d137472adf9f474ee4
Alfred Chillitupa Quispihuanca <77297648+Alfred-CQ@users.noreply.github.com>
2023-03-27 23:34:46 -0500

    💄 Improve the console output style (#4)
    * 💄 Improve the console output style
    
    [ include/yarl ]
      - [ scanner.py ] Updated to show token lines and <> symbols with colours 
      - [ token.py ] Tuple returned without <>
      - [ utils.py ] Added colours for console output
    
    * 🔥 Update console output style
    
    [ include/yarl ]
      - [ scanner.py ] Change from relative to absolute path and updated the function parameters
      - [ utils.py ] Improved display of error pointers and bug fixes in the colour functions

--- include/yarl/scanner.py
+++ include/yarl/scanner.py
@@ -1,6 +1,6 @@
 from yarl.definitions import Lexemes, Tag, lexeme_to_tag, compound_symbols
 from yarl.token import Token
-from yarl.utils import print_error
+from yarl.utils import print_error, prt_blue, prt_cyan, prt_red
 import os
 
 class Scanner:
@@ -23,10 +23,12 @@ class Scanner:
             for token in tokens:
                 if token.line != line:
                     line = token.line
-                    print()
-                print(token, end=" ")
-        print(f"\n\tFinishing scanning, there were {len(errors)} errors")
+                    print(f"\n   {line}{prt_blue(' |')}", end=" ")
+                print(f"{prt_cyan('<')}{token}{prt_cyan('>')}", end="  ")
 
+        print(f'\n  **** Finishing scanning, there were {prt_red(len(errors))} errors')
+
+          
     def __get_complete_str(self):
         colon_assign = False
         if self.__look_back_for(Lexemes.COLON_ASSIGN):
--- include/yarl/utils.py
+++ include/yarl/utils.py
@@ -3,8 +3,51 @@ def print_error(error):
     if error["content"][-1] == "\n":
         error["content"] = error["content"][:-1]
     print("\t", error["content"])
-    point_out_error = " " * error["idx_error"] + "^"
+    
+    idx_error = error["idx_error"]
+    highlight_number = 0 if "invalid" in error["msg"] else len(error["content"])-idx_error
+    point_out_error = " " * idx_error + prt_red("^" + "~" * highlight_number)
+    
     print("\t", point_out_error)
-    print("LexicalError: ", end="")
+    print("  LexicalError: ", end="")
     print(error["msg"])
     print()
+
+# Globals
+
+
+# Prettier Output
+BOLD   = '\33[1m'
+RED    = '\33[31m'
+GREEN  = '\33[32m'
+YELLOW = '\33[33m'
+BLUE   = '\33[34m'
+VIOLET = '\33[35m'
+BEIGE  = '\33[36m'
+WHITE  = '\33[37m'
+CYAN    = '\033[96m'
+RESET = '\033[00m'
+
+def prt_red(text):
+    return BOLD + RED + str(text) + RESET
+
+def prt_green(text):
+    return BOLD + GREEN + str(text) + RESET
+
+def prt_yellow(text):
+    return BOLD + YELLOW + str(text) + RESET
+
+def prt_blue(text): 
+    return BOLD + BLUE + str(text) + RESET
+
+def prt_violet(text): 
+    return BOLD + VIOLET + str(text) + RESET
+
+def prt_beige(text): 
+    return BOLD + BEIGE + str(text) + RESET
+
+def prt_white(text): 
+    return BOLD + WHITE + str(text) + RESET
+
+def prt_cyan(text):
+    return BOLD + CYAN + str(text) + RESET
